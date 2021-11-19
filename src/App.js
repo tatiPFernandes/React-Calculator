9594252c902a9b277ade946ad0e983c240d97a4e
Tati Fernandes <tatifernandes026@gmail.com>
2021-11-19 11:29:09 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from "react";
 
-import Wrapper from "../components/Wrapper";
-import Screen from "../components/Screen";
-import ButtonBox from "../components/ButtonBox";
-import Button from "../components/Button";
+import Wrapper from "./components/Wrapper";
+import Screen from "./components/Screen";
+import ButtonBox from "./components/ButtonBox";
+import Button from "./components/Button";
 
 const btnValues = [
   ["C", "+-", "%", "/"],
